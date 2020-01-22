b8ef43c03f6f476244208de8fbd9de25942134d7
Yousef <yahmed1996a@gmail.com>
2020-01-22 16:26:37 +0000

    Second mvp point done but n

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,11 +1,25 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/distilleries")
 public class DistilleryController {
 
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/regions/named/{name}")
+    public List<Distillery> findDistilleriesWithRegionNamed(@PathVariable String name){
+        return distilleryRepository.findDistilleriesByRegion(name);
+    }
 
 }
